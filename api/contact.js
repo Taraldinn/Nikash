b9aa666dfc90b5a52cf9231ce7c8ae607dad70b1
Taraldinn <kferdoush617@gmail.com>
2025-07-15 01:40:21 +0600

    Convert contact API to ES module syntax

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,6 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
